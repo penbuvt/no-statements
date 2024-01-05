e630a31139d3fc55cca2008ea2a1abb45f79871e
Penbu <penbu@penbuvt.ca>
2024-01-04 20:31:15 -0500

    Include full FizzBuzz implementation

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -10,6 +10,20 @@ function fizzBuzzSingle(n) {
   );
 }
 
+function fizzBuzz(n) {
+  return ((fizzBuzzSingle) => 
+    Array(n).fill(0).map((_, i) => fizzBuzzSingle(i + 1))
+  )((n) => ((multOf3, multOf5) =>
+    [
+      multOf3 && "Fizz",
+      multOf5 && "Buzz",
+      !multOf3 && !multOf5 && String(n),
+    ].filter(Boolean).join("")
+  )(n % 3 === 0, n % 5 === 0)
+  );
+}
+
 module.exports = {
   fizzBuzzSingle,
+  fizzBuzz,
 };
