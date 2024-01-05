646e4fc77698885d4a07d66b49156907d486e340
Penbu <penbu@penbuvt.ca>
2024-01-04 20:26:11 -0500

    Change FizzBuzz implementation to use array join

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,11 @@
 function fizzBuzzSingle(n) {
   return (
     ((multOf3, multOf5) =>
-      `${multOf3 ? "Fizz" : ""}${multOf5 ? "Buzz" : ""}${!multOf3 && !multOf5 ? String(n) : ""}`
+      [
+        multOf3 ? "Fizz" : "",
+        multOf5 ? "Buzz" : "",
+        !multOf3 && !multOf5 ? String(n) : "",
+      ].filter(Boolean).join("")
     )(n % 3 === 0, n % 5 === 0)
   );
 }
