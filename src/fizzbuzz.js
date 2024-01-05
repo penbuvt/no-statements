b859000b3ab7a724f156edda3353198fc689a155
Penbu <penbu@penbuvt.ca>
2024-01-04 20:17:22 -0500

    Change FizzBuzz implementation to continuation passing

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,11 +1,14 @@
 function fizzBuzzSingle(n) {
   return (
-    ((out) => (
-      (n % 3 === 0 ? out.push("Fizz") : void 0),
-      (n % 5 === 0 ? out.push("Buzz") : void 0),
-      (!out.length ? out.push(String(n)) : void 0),
-      out.join("")
-    ))([])
+    ((acc) => (
+      ((acc) => (
+        ((acc) => (
+          ((acc) => (
+            acc
+          ))(!acc.length ? String(n) : acc)
+        ))(n % 5 === 0 ? acc + "Buzz" : acc)
+      ))(n % 3 === 0 ? acc + "Fizz" : acc)
+    ))("")
   );
 }
 
